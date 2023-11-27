1970e735282f2c5992f238d76cd4022263a0f0a8
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-27 14:51:09 -0300

    Seller class.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(23, "Leandro", "leandro@email.com", new Date(), 4000.00, obj);
 
         System.out.println(obj);
-
+        System.out.println("\n" + seller);
     }
 }
