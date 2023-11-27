3ce55dac9c11e5c488ec4e7f4523b23a763ca384
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-27 15:17:56 -0300

    Dao Factory and SellerDAOJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(23, "Leandro", "leandro@email.com", new Date(), 4000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println("\n" + seller);
